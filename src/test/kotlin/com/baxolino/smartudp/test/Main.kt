52fb10e900c33f7019f94f0b02b32f209e070c81
Kumaraswamy B G <care.xoma@gmail.com>
2025-01-24 12:22:12 +0530

    uwu

--- src/test/kotlin/com/baxolino/smartudp/test/Main.kt
+++ src/test/kotlin/com/baxolino/smartudp/test/Main.kt
@@ -6,12 +6,6 @@ import java.net.InetAddress
 object Main {
   @JvmStatic
   fun main(args: Array<String>) {
-    SmartUDP
-      .create(2244)
-      .message(InetAddress.getByName("localhost"), 1234, "hello world".toByteArray(), "uwu")
-      .handleResponse("uwu") {
-        println("Received reply: ${String(it)}")
-        null
-      }
+
   }
 }
